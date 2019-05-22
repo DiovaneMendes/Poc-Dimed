d178b0f7d3a5cce04f4e558942ebb1a0f58389c9
diovanemendes <diovane.m.mendes@gmail.com>
2019-05-22 18:00:20 -0300

    Modifica nomeclatura duplicada e implementa consumo de api com token

--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Item } from '../model/item';
 
 @Injectable()
-export class ItemServiceService {
+export class ItemService implements HttpInterceptor{
 
   constructor(private http: HttpClient) { }
 
-  buscaItem(nome): Observable<Item[]>{
+  buscaItens(nome): Observable<Item[]>{
     const url: string = `http://tst.grupodimedservices.com.br/item/v3/itens/base/autocomplete?nome=${nome}&codigoFilial=101&maxResult=200&ordenarRentabilidade=true&ordenarPreco=false`;
 
     return this.http.get<Item[]>(url);
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const dupReq = req.clone({ headers: req.headers.set('app-token', 'mCl6SnTQp6eT')});
+
+    return next.handle(dupReq);
   }
 
 }
