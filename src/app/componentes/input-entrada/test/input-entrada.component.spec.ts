6682de07fa01b24b6af00a3d6e22b5f0d2e192e8
diovanemendes <diovane.m.mendes@gmail.com>
2019-06-03 13:04:32 -0300

    Adiciona caso de teste para component input

--- src/app/componentes/input-entrada/test/input-entrada.component.spec.ts
+++ src/app/componentes/input-entrada/test/input-entrada.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InputEntradaComponent } from '../input-entrada.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-
 describe('InputEntradaComponent', () => {
   let component: InputEntradaComponent;
   let fixture: ComponentFixture<InputEntradaComponent>;
@@ -19,11 +18,23 @@ describe('InputEntradaComponent', () => {
     .then(() => {
       fixture = TestBed.createComponent(InputEntradaComponent);
       component = fixture.componentInstance;
+      component.descricao = 'tors';
       fixture.detectChanges();
     });
   }));
 
   it('Deve criar component', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Dado que o metodo [resposta] seja chamado... ', () => {
+    beforeEach(() => {
+      spyOn(component.enviaDescricao, 'emit');
+      component.resposta();
+    });
+
+    it('Deve emitir descrição', () => {
+      expect(component.enviaDescricao.emit).toHaveBeenCalledWith('tors');
+    });
   });
 });
